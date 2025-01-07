398cea44b5c424aa8d552c59da52dd618cbd29e1
osmancano <canohibro@gmail.com>
2025-01-07 16:18:47 +0300

    Basic Images end point with get and post methods

--- api/main.py
+++ api/main.py
@@ -1,8 +1,13 @@
 import os
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
+
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -23,7 +28,7 @@ app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
-    word = request.args.get("query")-a
+    word = request.args.get("query")
     headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
@@ -31,6 +36,19 @@ def new_image():
     data = response.json()
     return data
 
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        # read images from the database
+        images = images_collection.find({})
+        return jsonify([img for img in images])
+    if request.method == "POST":
+        # save image in the database
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return  {"inserted_id": inserted_id}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
